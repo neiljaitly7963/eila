130bdb659aac6bcb52199d4bcfab70118f2afc72
neiljaitly7963 <neiljaitly7963@gmail.com>
2020-08-11 20:59:41 +0530

    Updates

---
+++ precache-manifest.36f669cefb6d7e86ebd673528e9ddba3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b431280913396695ac20895f50009609",
+    "url": "/eila/index.html"
+  },
+  {
+    "revision": "ef8e0f2e5206716849eb",
+    "url": "/eila/static/css/2.3e7f680e.chunk.css"
+  },
+  {
+    "revision": "74b42da49952f6b90234",
+    "url": "/eila/static/css/main.98ebc7e0.chunk.css"
+  },
+  {
+    "revision": "ef8e0f2e5206716849eb",
+    "url": "/eila/static/js/2.f06581bc.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/eila/static/js/2.f06581bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74b42da49952f6b90234",
+    "url": "/eila/static/js/main.05626119.chunk.js"
+  },
+  {
+    "revision": "2ace3373826ab15db6e3",
+    "url": "/eila/static/js/runtime-main.9c518d37.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/eila/static/media/logo.5d5d9eef.svg"
+  }
+]);
